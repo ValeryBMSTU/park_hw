6d25f76e64181431b21d2a72c6b80c260992de0e
ValeryBMSTU <30847068+ValeryBMSTU@users.noreply.github.com>
2019-03-09 02:02:26 +0300

    Update IZ1_Shulman.c

--- IZ1_Shulman.c
+++ IZ1_Shulman.c
@@ -12,17 +12,21 @@
 #include <string.h>
 
 #define BUF_SIZE 256
-#define ASCII_SHIFT 32
-#define TRUE 1
+#define STRINGS_SIZE 4
+#define TRUE 1 // Мой С90 не поддерживает bool
+#define FALSE 0 
 
 int reg_down(char** const source_strings, const int count, char*** new_strings);
 
-int resize(char*** st, const char* buffer, const int count);
+int resize(char*** strings, const char* const buffer, const int count);
+
+int string_lwr(char *new_string, const char* const source_string);
 
 int main() {
-	int count = 0;
-	int error_flag = 0;
+	int count = 0, k = 0;
+	int error_flag = FALSE;
 	char** strings = NULL;
+	char** result_strings = NULL;
 
 	for (char buffer[BUF_SIZE] = { "\0" }; error_flag != 1; count++) {
 		if (fgets(buffer, BUF_SIZE, stdin) == NULL)	{
@@ -33,31 +37,35 @@ int main() {
 		}
 		error_flag = resize(&strings, buffer, count);
 	}
-	if ((count < 1) || (error_flag == TRUE)) {
+	if ((count < 1) || error_flag) {
 		printf("[error]");
 	}
 	else {
-		char** result_strings = NULL;
-		const int k = reg_down(strings, count, &result_strings);
-
-		for (int i = 0; i < k; i++) {
-			printf("%s", result_strings[i]);
-			free(result_strings[i]);
+		k = reg_down(strings, count, &result_strings);
+		if (count == k)	{
+			for (int i = 0; i < k; i++) {
+				printf("%s", result_strings[i]);
+			}
 		}
-		free(result_strings);
+		else {
+			printf("[error]");
+		}
 	}
+
+	for (int i = 0; i < k; i++)	{
+		free(result_strings[i]);
+	}
+	free(result_strings);
 	for (int i = 0; i < count; i++) {
 		free(strings[i]);
 	}
 	free(strings);
 
-	getchar();
 	return 0;
 }
 
 int reg_down(char** const source_strings, const int count, char*** new_strings) {
-	if (new_strings == NULL)
-	{
+	if (new_strings == NULL)	{
 		return -1;
 	}
 
@@ -73,23 +81,25 @@ int reg_down(char** const source_strings, const int count, char*** new_strings) 
 			break;
 		}
 
-		for (int j = 0; source_strings[i][j] != '\0'; j++)
-			if ((source_strings[i][j] >= 'A') && (source_strings[i][j] <= 'Z')) {
-				(*new_strings)[i][j] = source_strings[i][j] + ASCII_SHIFT;
-			}
-			else {
-				(*new_strings)[i][j] = source_strings[i][j];
-			}
-		(*new_strings)[i][strlen(source_strings[i])] = '\0';
+		if (string_lwr((*new_strings)[i], source_strings[i]) != strlen(source_strings[i]))	{
+			break;
+		}
 	}
 	return i;
 }
 
-int resize(char*** strings, const char* buffer, const int count) {
-	char** p = (*strings);
-	(*strings) = (char**)realloc(p, sizeof(char*) * (count + 1));
-	if ((*strings) == NULL) {
-		return -1;
+int resize(char*** strings, const char* const buffer, const int count) {
+
+	static int curent_size = STRINGS_SIZE;
+
+	if (count == curent_size || count == 0)
+	{
+		curent_size *= 2;
+		char** p = (*strings);
+		(*strings) = (char**)realloc(p, sizeof(char*) * (curent_size));
+		if ((*strings) == NULL) {
+			return -1;
+		}
 	}
 
 	(*strings)[count] = (char*)malloc(sizeof(char) * (strlen(buffer) + 1));
@@ -102,4 +112,18 @@ int resize(char*** strings, const char* buffer, const int count) {
 	}
 	(*strings)[count][strlen(buffer)] = '\0';
 	return 0;
+}
+
+int string_lwr(char *new_string, const char* const source_string)
+{
+	int j = 0;
+	for (; source_string[j] != '\0'; j++)
+		if ((source_string[j] >= 'A') && (source_string[j] <= 'Z')) {
+			new_string[j] = (source_string[j] - 'A') + 'a';
+		}
+		else {
+			new_string[j] = source_string[j];
+		}
+	new_string[strlen(source_string)] = '\0';
+	return j;
 }
